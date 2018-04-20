51334dbf2e5c02b349b7b5ac1606db897d49e654
aleruiz <alejandro@numerated>
2018-04-19 21:25:13 -0400

    WIP

--- main.js
+++ main.js
@@ -33,22 +33,32 @@ $( document ).ready(function(){
     });
   }
 
-  // simple routing
+
+  
+  // ----------- simple routing -------------
+ 
   var toggle_page = function(page_name) {
     window.location.hash = "#"+ page_name;
     $(".content-container").load("/pages/" + page_name + ".html");
-//    $(location).attr(
-//    window.location.reload();
   }  
   $(".navbarlink").click(function(e) {
     e.preventDefault();
     toggle_page(e.target.id);
   });
 
+  if (!("onhashchange" in window)) {
+    alert("You're running an old browser! This Cold House won't work very well, though I'll do my best. Please consider upgrading for the best experience!")
+  }
+  
+  locationHasChanged = function(){    
+    toggle_page(window.location.hash.substring(1));
+  };
+  window.onhashchange = locationHasChanged;
+
+  
   // set up the page 
   // actually injects the right html in our simple routing scheme
-
-  var page = window.location.href.split("#")
+  var page = window.location.hash.substring(1);
   if (page.length != 2 || !page[1]){
     page = "/pages/home.html";
   }
