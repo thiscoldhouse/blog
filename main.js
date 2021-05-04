d30b501fd333bc5f745ae06246bc54246140f2d2
aleruiz <ales.email@gmail.com>
2021-05-04 15:41:06 -0400

    Clean up routing, fix css firefox bug in apportionment

--- main.js
+++ main.js
@@ -1,44 +1,45 @@
 var blog = blog || {};
 
 $( document ).ready(function(){
+  // ----------- simple routing ------------- //
 
-//    $(
+  if (!("onhashchange" in window)) {
+    alert("You're running an old browser! This Cold House won't work very well, though I'll do my best. Please consider upgrading for the best experience!")
+  }
 
-    // ----------- simple routing -------------
 
-    var toggle_page = function(page_name) {
-        window.location.hash = "#"+ page_name;
-        $(".content-container").load("/pages/" + page_name + ".html");
-    }  ;
-    $(".navbarlink").click(function(e) {
-        e.preventDefault();
-        toggle_page(e.target.id);
-    });
+  function toggle_page(page_name) {
+    window.location.hash = "#"+ page_name;
+    $(".content-container").load("/pages/" + page_name + ".html");
+  }
 
-    if (!("onhashchange" in window)) {
-        alert("You're running an old browser! This Cold House won't work very well, though I'll do my best. Please consider upgrading for the best experience!")
-    }
+  function locationHasChanged(){
+    toggle_page(window.location.hash.substring(1));
+  };
 
-    var locationHasChanged = function(){
-        toggle_page(window.location.hash.substring(1));
-    };
+
+  function attachHandlers(){
     window.onhashchange = locationHasChanged;
 
+    $(".navbarlink").click(function(e) {
+      e.preventDefault();
+      toggle_page(e.target.id);
+    });
+  }
 
-    // set up the page
-    // actually injects the right html in our simple routing scheme
+  function setupPage(){
+    attachHandlers();
+
     var page = window.location.hash.substring(1);
-    if (page.length != 2 || !page[1]){
-        page = "/pages/projects.html";
+
+    // handle homepage edge case
+    if (!page){
+      toggle_page('projects');
     }
-    else {
-        page = page[1];
-        page = "/pages/" + page + ".html";
-    };
-
-    $(".content-container").load(page);
-
-    // start up the site
     locationHasChanged();
+
+  }
+
+  setupPage();
 
 });
