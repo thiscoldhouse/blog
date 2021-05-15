512b513819d88d2f1acb8527fee3908cb05b8462
aleruiz <ales.email@gmail.com>
2021-05-15 18:10:52 -0400

    Add image modal

--- main.js
+++ main.js
@@ -18,17 +18,52 @@ $( document ).ready(function(){
   };
 
 
-  function attachHandlers(){
+  function attachNavHandlers(){
     window.onhashchange = locationHasChanged;
 
     $(".navbarlink").click(function(e) {
       e.preventDefault();
       toggle_page(e.target.id);
     });
+
   }
 
+  function attachModalHandlers(){
+    function handleModalButtonClick(e){
+        let image = $(e.currentTarget).attr('src');
+        $('#modal').css('display', 'block');
+        //$('#modal').css('background-image', 'url(' + image + ')');
+        $('#modal-image').attr('src', image);
+
+
+        // make a centered square
+        let offset = $(document).scrollTop();
+        let viewportHeight = $(window).height();
+        let viewportWidth = $(window).width();
+        $('#modal').css('width', viewportWidth/2 + 'px');
+        $('#modal').css('left', (viewportWidth/4) + 'px');
+
+        // adjust close modal button dynamically
+        let imageLeft = $('#modal-image').position()['left'];
+        let imageWidth = $('#modal-image').width()
+        $('#close-modal').css('left', imageLeft + imageWidth - 3);
+    }
+
+    $('.modal-btn').each(function(i){
+      modalButton = $($('.modal-btn')[i])
+      console.log(modalButton);
+      $(modalButton).click(handleModalButtonClick);
+
+    });
+    $('#close-modal').click(function(e){
+      $('#modal').css('display', 'none');
+    });
+  }
+
+  blog['attachModalHandlers'] = attachModalHandlers;
+
   function setupPage(){
-    attachHandlers();
+    attachNavHandlers();
 
     var page = window.location.hash.substring(1);
 
